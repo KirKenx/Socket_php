4680556f952c28e36fb78ee1a1e0a4eea0fb7091
KirKenx <ngothanhcong98@gmail.com>
2020-03-06 16:08:47 +0700

    test1

--- server.php
+++ server.php
@@ -1,1 +1,11 @@
-server.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width="30px", initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
