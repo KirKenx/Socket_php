2f14472594d2a3b6c815754864aa087837280e11
KirKenx <ngothanhcong98@gmail.com>
2020-03-10 17:12:54 +0700

    socket chat

--- client.php
+++ client.php
@@ -2,10 +2,33 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viesasdasdwport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <p>afasfssdgfdg</p>
+    <form method="POST">
+    <input type="text" name="message">
+    <input type="submit" name="btnSend">
+    </form>
 </body>
 </html>
+<?php
+$host    = "127.0.0.1";
+$port    = 1111;
+
+// echo "Message To server :".$message;
+// create socket
+if ( isset($_POST['btnSend']) ) {
+    $message = $_POST['message'];
+    $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+    // connect to server
+    $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
+    // send string to server
+    socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+    // get server response
+    $result = socket_read ($socket, 1024) or die("Could not read server response\n");
+    echo "Reply From Server  :".$result;
+    // close socket
+    socket_close($socket);
+}
+?>
--- server.php
+++ server.php
@@ -1,11 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width="30px", initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p> gi cung duoc  ok</p>
-</body>
-</html>
+<?php 
+$host = "127.0.0.1";
+$port = 1111;
+
+set_time_limit(0);
+// create socket
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+// bind socket to port
+$result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
+// start listening for connections
+$result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
+class Chat{
+    function Readline(){
+        return rtrim(fgets(STDIN));
+    }
+}
+do{
+        // accept incoming connections
+    // spawn another socket to handle communication
+    $spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
+    // read client input
+    $input = socket_read($spawn, 1024) or die("Could not read input\n");
+    // clean up input string
+    $input = trim($input);
+    if ($input== "1") break;
+    echo "Client Message : ".$input."<br>";
+    // $line = new Chat();
+    // echo("Enter reply\t ");
+    // $reply = $line-->Readline();
+    
+    // reverse client input and send back
+    $reply = strrev($input) . "\n";
+    socket_write($spawn, $reply, strlen ($reply)) or die("Could not write output\n");
+    // close sockets
+    
+}while ( true);
+socket_close($spawn,$socket);
+?>
