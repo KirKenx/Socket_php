903964cd23c9902e5c88ff93b2cc8c372a03425d
KirKenx <ngothanhcong98@gmail.com>
2020-03-12 14:39:23 +0700

    1

--- client.php
+++ client.php
@@ -13,13 +13,39 @@
 </body>
 </html>
 <?php
+function rc4($str, $key) {
+	$s = array();
+	for ($i = 0; $i < 256; $i++) {
+		$s[$i] = $i;
+	}
+	$j = 0;
+	for ($i = 0; $i < 256; $i++) {
+		$j = ($j + $s[$i] + ord($key[$i % strlen($key)])) % 256;
+		$x = $s[$i];
+		$s[$i] = $s[$j];
+		$s[$j] = $x;
+	}
+	$i = 0;
+	$j = 0;
+	$res = '';
+	for ($y = 0; $y < strlen($str); $y++) {
+		$i = ($i + 1) % 256;
+		$j = ($j + $s[$i]) % 256;
+		$x = $s[$i];
+		$s[$i] = $s[$j];
+		$s[$j] = $x;
+		$res .= $str[$y] ^ chr($s[($s[$i] + $s[$j]) % 256]);
+	}
+	return $res;
+}
 $host    = "127.0.0.1";
 $port    = 1111;
 
 // echo "Message To server :".$message;
 // create socket
 if ( isset($_POST['btnSend']) ) {
-    $message = $_POST['message'];
+    $tn = $_POST['message'];
+    $message = rc4($tn,"key");
     $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
     // connect to server
     $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
--- server.php
+++ server.php
@@ -3,6 +3,34 @@ $host = "127.0.0.1";
 $port = 1111;
 
 set_time_limit(0);
+
+
+function rc4($str, $key) {
+	$s = array();
+	for ($i = 0; $i < 256; $i++) {
+		$s[$i] = $i;
+	}
+	$j = 0;
+	for ($i = 0; $i < 256; $i++) {
+		$j = ($j + $s[$i] + ord($key[$i % strlen($key)])) % 256;
+		$x = $s[$i];
+		$s[$i] = $s[$j];
+		$s[$j] = $x;
+	}
+	$i = 0;
+	$j = 0;
+	$res = '';
+	for ($y = 0; $y < strlen($str); $y++) {
+		$i = ($i + 1) % 256;
+		$j = ($j + $s[$i]) % 256;
+		$x = $s[$i];
+		$s[$i] = $s[$j];
+		$s[$j] = $x;
+		$res .= $str[$y] ^ chr($s[($s[$i] + $s[$j]) % 256]);
+	}
+	return $res;
+}
+
 // create socket
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 // bind socket to port
@@ -21,6 +49,7 @@ do{
     // read client input
     $input = socket_read($spawn, 1024) or die("Could not read input\n");
     // clean up input string
+    $input=rc4($input,"key");
     $input = trim($input);
     if ($input== "1") break;
     echo "Client Message : ".$input."<br>";
